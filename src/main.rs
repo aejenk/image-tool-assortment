a54b813a1f429aa730e7654f63c3353265ab57c6
ajfyb <mctech26@gmail.com>
2023-08-05 16:58:24 +0200

    Updated palette template

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use image_filters::{
-    hsl_gradient_map,
-    pixel::{hsl::HslPixel, rgb::colours as RGB},
+    pixel::{rgb::{colours as RGB, RgbPixel}, oklch::OklchPixel},
     prelude::*,
     utils::{
         image::load_image_from_url_with_max_dim,
@@ -11,53 +10,72 @@ use image_filters::{
 // this file is essentially for testing / running the code, more than providing an actual reusable binary
 
 fn main() -> ImageFilterResult<()> {
-    let _gradient = hsl_gradient_map![
-        0.00 => sat: 0.0, lum: 0.0, hue: 0.0,
-        0.30 => sat: 0.8, lum: 0.3, hue: 280.0,
-        0.60 => sat: 0.8, lum: 0.6, hue: 200.0,
-        0.80 => sat: 0.8, lum: 0.8, hue: 40.0,
-        1.00 => sat: 0.0, lum: 1.0, hue: 260.0
+    // let build_gradient = RgbPixel::build_gradient_using_hsl;
+    let build_gradient = RgbPixel::build_gradient_using_oklch;
+
+    let mut palettes = vec![
+        (
+            "pastel",
+            [
+                build_gradient(&RGB::CYAN.mix(0.8, &RGB::BLUE), 10),
+                build_gradient(&RGB::PINK.mix(0.8, &RGB::RED), 10),
+                vec![RGB::BLACK, RGB::WHITE],
+            ]
+            .concat(),
+        ),
+        (
+            "carrot",
+            [
+                build_gradient(&RGB::ORANGE, 10),
+                build_gradient(&RGB::GREEN, 10),
+                vec![RGB::BLACK, RGB::WHITE],
+            ]
+            .concat(),
+        ),
+        (
+            "nb",
+            [
+                build_gradient(&RGB::GOLD, 10),
+                build_gradient(&RGB::PURPLE, 10),
+                vec![RGB::BLACK, RGB::WHITE],
+            ]
+            .concat(),
+        ),
+        (
+            "sunsky",
+            [
+                build_gradient(&RGB::ORANGE, 10),
+                build_gradient(&RGB::BLUE.mix(0.2, &RGB::CYAN), 10),
+                vec![RGB::BLACK, RGB::WHITE],
+            ]
+            .concat(),
+        ),
+        (
+            "cosmos",
+            [
+                build_gradient(&RGB::BLUE, 10),
+                build_gradient(&RGB::PURPLE, 10),
+                build_gradient(&RGB::ROSE, 10),
+                vec![RGB::BLACK, RGB::WHITE],
+            ]
+            .concat(),
+        ),
     ];
 
-    let pastel_palette = (
-        "pastel",
-        [
-            RGB::CYAN.mix(0.7, &RGB::BLUE).build_gradient(10),
-            RGB::PINK.mix(0.7, &RGB::RED).build_gradient(10),
-        ]
-        .concat(),
-    );
+    palettes.push((
+        "all",
+        palettes.iter().map(|col| (&col.1).clone()).collect::<Vec<_>>().concat(),
+    ));
 
-    let rust_palette = (
-        "rust",
-        [
-            RGB::ORANGE.mix(0.2, &RGB::RED).build_gradient(5),
-            RGB::ORANGE.mix(0.5, &RGB::RED).build_gradient(5),
-            RGB::ORANGE.mix(0.9, &RGB::RED).build_gradient(5),
-            vec![RGB::BLACK],
-        ]
-        .concat(),
-    );
-
-    let nb_palette = (
-        "nb",
-        [
-            RGB::GOLD.build_gradient(3),
-            RGB::PURPLE.build_gradient(10),
-            RGB::GOLD.build_gradient_mix(&RGB::PURPLE, 10),
-            vec![RGB::BLACK, RGB::WHITE],
-        ]
-        .concat(),
-    );
-
-    let palettes = [&pastel_palette, &rust_palette, &nb_palette];
-
-    let link_to_image = "https://scied.ucar.edu/sites/default/files/styles/half_width/public/2021-10/cumulus-clouds.jpg.webp?itok=HkQfuWxM";
+    let link_to_image = "https://www.bibalex.org/SCIplanet/Attachments/images/big-orange-sun-4.jpg";
     let image = load_image_from_url_with_max_dim(link_to_image, 1080)?.apply(Filter::Contrast(1.3));
+
+    image.save("data/_original.png")?;
 
     for (name, palette) in palettes.into_iter() {
         image
             .clone()
+            // .apply(Filter::Contrast( 1.2))
             .apply(Dither::Bayer(8, &palette))
             .save(format!("data/output-{}.png", name))?;
     }
